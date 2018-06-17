b4611da6bff1a66a401bd4f55887ecc5aa69ba9a
shubhang.sabbath@gmail.com <ravitejvaranasi@bitbucket.org>
2018-06-17 20:51:02 +0530

    fix: tests

--- src/test/java/SqlServiceTest.java
+++ src/test/java/SqlServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.dao.InvalidDataAccessApiUsageException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
@@ -20,16 +21,22 @@ public class SqlServiceTest {
 
     private DataSource ds;
     private SqlService sqlService;
+    private Boolean isSetupDone = false;
 
     @Before
     public void setup() {
-        ds = new JdbcDataSource();
-        ((JdbcDataSource) ds).setURL("jdbc:h2:./src/test/resource/db/test");
-        ((JdbcDataSource) ds).setUser("sa");
-        ((JdbcDataSource) ds).setPassword("sa");
-        initializeSqlService();
-        setupTables();
-        setupData();
+        System.out.println("Is Set up done -->" + isSetupDone);
+        if (!isSetupDone) {
+            System.out.println("Set up...");
+            ds = new JdbcDataSource();
+            ((JdbcDataSource) ds).setURL("jdbc:h2:./src/test/resource/db/test");
+            ((JdbcDataSource) ds).setUser("sa");
+            ((JdbcDataSource) ds).setPassword("sa");
+            initializeSqlService();
+            setupTables();
+            setupData();
+            isSetupDone = true;
+        }
     }
 
     private void initializeSqlService() {
@@ -41,9 +48,13 @@ public class SqlServiceTest {
         try {
             Connection conn = ds.getConnection();
             Statement stmt = conn.createStatement();
-            stmt.execute("drop table product");
-            stmt.execute("create table product (id int primary key , code varchar(20),name varchar(20))");
-            conn.close();
+            DatabaseMetaData dbm = conn.getMetaData();
+            if (!isSetupDone) {
+                stmt.execute("create table product (id int primary key , code varchar(20),name varchar(20))");
+                conn.close();
+            }
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -53,6 +64,7 @@ public class SqlServiceTest {
         try {
             Connection conn = ds.getConnection();
             Statement stmt = conn.createStatement();
+
             stmt.execute("insert into product values(1,'p234','soap')");
             stmt.execute("insert into product values (2,'p456','soup')");
             conn.close();
